0426e6a33325f00d4527481de07e086f38c42af9
Caprissimo Forte <popescu.cristian2002@yahoo.com>
2024-02-29 18:15:25 +0200

    add lazy loading to service span images

--- src/components/service-span.js
+++ src/components/service-span.js
@@ -5,7 +5,7 @@ import { useLocale, useTranslation } from "../module/i18n.js";
 /*import { useState } from "react";*/
 import { CallToActionButton } from "../components/call-to-action.js";
 
-function ServiceSpan({ children, id, image, title }) {
+function ServiceSpan({ children, id, image, title, loading }) {
     const locale = useLocale();
     const translate = useTranslation();
 
@@ -21,7 +21,7 @@ function ServiceSpan({ children, id, image, title }) {
     return (
         <span id={id} className="service-span-wrapper">
             <div className="service-span-image-wrapper">
-                <img loading="lazy" src={image} alt=""/>
+                <img loading={loading} src={image} alt=""/>
             </div>
             <div className="service-span">
                 <div className="service-span-title-wrapper">
@@ -38,26 +38,5 @@ function ServiceSpan({ children, id, image, title }) {
         </span>
     );
 }
-
-/*
-                <div className="service-span-children-wrapper" style={{gridTemplateRows: expanded ? "1fr" : "0fr" }}>
-                    <div className="service-span-children">
-                        {children}
-                    </div>
-                </div>
-                <CallToActionButton onClick={handleClick}>
-                    {!expanded ?
-                        <>
-                        <p>Vezi mai mult</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z"/></svg>
-                        </>
-                    :
-                        <>
-                        <p>Vezi mai puțin</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z"/></svg>
-                        </>
-                    }
-                </CallToActionButton>
- */
 
 export default ServiceSpan;
